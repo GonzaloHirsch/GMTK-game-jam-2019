5132e31a99f045938bd74ba9ec04519b30891750
Nacho Villanueva <ivillanueva@itba.edu.ar>
2019-08-03 14:30:13 -0300

    Added planning stage code

---
+++ One Chance Plan/Assets/Scripts/Planning Stage/PlanningController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanningController : MonoBehaviour
+{
+    private Queue<Action> ActionQueue;
+
+    public void AddWait()
+    {
+        ActionQueue.Enqueue(new Action(){ })
+    }
+
+    public void AddInteraction()
+    {
+
+    }
+
+    public void AddMovement()
+    {
+
+    }
+
+}
