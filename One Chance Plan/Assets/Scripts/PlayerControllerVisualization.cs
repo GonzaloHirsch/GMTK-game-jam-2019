7a82657fa9e356f32c177898a1913f3f679a4cdb
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-08-03 12:28:37 -0300

    Added movement

---
+++ One Chance Plan/Assets/Scripts/PlayerControllerVisualization.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerVisualization : MonoBehaviour
+{
+    public float Velocity = 2f;
+    public float ViewingDistance = 4f;
+
+    private Vector3 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitVariables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDirection();
+
+        UpdateMovement();
+
+        UpdateView();
+    }
+
+    private void InitVariables()
+    {
+        this.direction = new Vector3
+        {
+            z = 0f
+        };
+    }
+
+    private void UpdateDirection()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void UpdateMovement()
+    {
+        transform.position += direction.normalized * Time.deltaTime * Velocity;
+    }
+
+    private void UpdateView()
+    {
+        RaycastHit2D raycast = Physics2D.Raycast(ToVector2(transform.position), direction.normalized, ViewingDistance);
+
+        HandleRaycast(raycast);
+
+        Debug.DrawRay(ToVector2(transform.position), direction.normalized * ViewingDistance, Color.green, 10f);
+    }
+
+    /// <summary>
+    /// Handles the raycast when the player is moving between the fog.
+    /// In case the ray hits a gameobject tagged with "Fog", it's destroyed.
+    /// </summary>
+    /// <param name="raycastHit">Raycast hit.</param>
+    private void HandleRaycast(RaycastHit2D raycastHit)
+    {
+        if (raycastHit.collider != null && raycastHit.collider.gameObject.tag.Equals("Fog"))
+        {
+            Destroy(raycastHit.collider.gameObject);
+        }
+    }
+
+    private Vector2 ToVector2(Vector3 vector)
+    {
+        return new Vector2()
+        {
+            x = vector.x,
+            y = vector.y
+        };
+    }
+}
---
+++ One Chance Plan/Assets/Scripts/PoliceControllerVisualization.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PoliceControllerVisualization : MonoBehaviour
+{
+    public List<Vector2> positions;
+    public bool isActive = true;
+    public float velocity = 3f;
+    public float smoothFactor = 0.75f;
+
+    private int positionsIndex = 0;
+    private Tweener activeTweener;
+    private float timeBewtweenMovements;
+    private Vector3 nextPosition;
+    private float distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DOTween.Init();
+
+        nextPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive && !AreSameVectors(nextPosition, transform.position))
+        {
+            UpdateMovement();
+        } else if (AreSameVectors(nextPosition, transform.position))
+        {
+            NextMovement();
+        }
+
+        /*
+        if (isActive && ((activeTweener != null && activeTweener.IsComplete()) || activeTweener == null))
+        {
+            UpdateMovement();
+        }
+        */
+    }
+
+    private void UpdateMovement()
+    {
+        float step = velocity * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, step);
+
+        //transform.position = Vector3.Lerp(transform.position, nextPosition, smoothFactor * Time.deltaTime / distance);
+
+        /*
+        timeBewtweenMovements = CalculateTime(transform.position, transform.position + ToVector3(positions[positionsIndex]));
+
+        Debug.Log(timeBewtweenMovements);
+
+        activeTweener = transform.DOMove(transform.position + ToVector3(positions[positionsIndex]), timeBewtweenMovements, true)
+            .OnComplete(() => {
+            activeTweener = null;
+        }
+
+                );
+
+*/
+        //positionsIndex = (positionsIndex + 1) % positions.Count;
+    }
+
+    private void NextMovement()
+    {
+        nextPosition = transform.position + ToVector3(positions[positionsIndex]);
+
+        distance = Mathf.Abs((transform.position - nextPosition).magnitude);
+
+        positionsIndex = (positionsIndex + 1) % positions.Count;
+    }
+
+    private float CalculateTime(Vector3 start, Vector3 end)
+    {
+        return (end - start).magnitude / velocity;
+    }
+
+    private void UpdateView()
+    {
+    }
+
+    private bool AreSameVectors(Vector3 a, Vector3 b)
+    {
+        return Mathf.Abs((a - b).magnitude) < 0.05f;
+    }
+
+    private Vector3 ToVector3(Vector2 vector)
+    {
+        return new Vector3()
+        {
+            x = vector.x,
+            y = vector.y,
+            z = 0
+        };
+    }
+}
